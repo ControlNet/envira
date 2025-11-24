ff7b3d1658505205929c56ccafd2a7588bbf6d50
ControlNet <smczx@hotmail.com>
2025-11-24 13:50:58 +1100

    portainer agent is useless, disabled

--- run.sh
+++ run.sh
@@ -444,7 +444,7 @@ systemctl --user start jupyter.service
 
 # run portainer agent
 # disable as it requires docker which might not feasible in some cases
-sudo docker run -d -p 9001:9001 --name portainer_agent --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes portainer/agent:latest
+# sudo docker run -d -p 9001:9001 --name portainer_agent --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes portainer/agent:latest
 
 cargo cache -a
 
